4d7149893185dfc7cf2c4b964713b2c82000d10d
paraiso3d <normanparaiso.abm12@gmail.com>
2025-11-13 19:54:10 +0800

    AA

--- app/Http/Controllers/EmployeeController.php
+++ app/Http/Controllers/EmployeeController.php
@@ -96,7 +96,7 @@ class EmployeeController extends Controller
             $emp->allowances = $emp->employeeAllowances->map(fn($ea) => [
                 'id'          => $ea->allowance->id,
                 'type_name'   => $ea->allowance->type_name,
-                'amount'       => $ea->amount, // actual assigned amount
+                'amount'       => $ea->amount,
                 'description' => $ea->allowance->description,
             ]);
 
